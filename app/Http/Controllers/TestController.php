6c0d878ff9790f5f5b0078f39b7dde34da39e3a0
Hiro-0403 <63594128+Hiro-0403@users.noreply.github.com>
2020-11-30 23:31:17 +0900

    laravel MVC データベース
    データベースから取り出して、ビューを表示。

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Test;
+
 class TestController extends Controller
 {
     //
     public function index()
     {
-        return view("tests.test");
+        $values = Test::all();
+
+        // dd($values);
+        #処理を止めて変数の中身をみることができる。
+
+        return view("tests.test", compact("values"));
     }
 }
--- resources/views/tests/test.blade.php
+++ resources/views/tests/test.blade.php
@@ -1,1 +1,7 @@
 test
+
+@foreach($values as $value)
+<!-- 複数形と単数形が一般的 -->
+{{$value->id}}<br>
+{{$value->text}}<br>
+@endforeach
